f35634ca04353500866f9fa097316dbb06727184
mikaeltv33n <122784949+mikaeltv33n@users.noreply.github.com>
2023-09-01 13:54:15 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,57 @@
-// skriv din JavaScript her...
+const TIME = document.querySelector("#time")
+const GREETING = document.querySelector("#greeting")
+
+
+function showTime() {
+    let today = new Date()
+    hour = today.getHours()
+    min = today.getMinutes()
+    sec = today.getSeconds()
+
+    hour = hour % 24 || 24
+
+    TIME.innerHTML = `${addZero(hour)}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`
+
+    setTimeout(showTime, 1000)
+}
+
+function addZero(n) {
+    return (parseInt(n, 10) < 10 ? '0' : '') + n
+}
+
+function setGreeting() {
+    let today = new Date()
+    hour = today.getHours()
+
+    if (hour < 9) {
+        // if the hour is less than 9 it's morning
+        document.body.style.backgroundImage = "url('./pics/morning.avif')"
+        GREETING.textContent = 'God Morgen'
+         document.body.style.color = 'black'
+
+    } else if (hour < 12) {
+        // if the hour is less than 12 it's noon
+        document.body.style.backgroundImage = "url('./pics/noon.avif')"
+        GREETING.textContent = 'God formiddag'
+        document.body.style.color = 'black'
+
+    } else if (hour < 18) {
+        // if the hour is less than 18 it's afternoon 
+        document.body.style.backgroundImage = "url('./pics/afternoon.avif')"
+        GREETING.textContent = 'God eftermiddag'
+        document.body.style.color = 'white'
+         
+
+    } else {
+        // it's evening 
+        document.body.style.backgroundImage = "url('./pics/night.avif')"
+        GREETING.textContent = 'God aften'
+        document.body.style.color = 'white'
+
+        
+    }
+}
+
+
+showTime()
+setGreeting()
